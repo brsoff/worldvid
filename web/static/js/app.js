066119510c4ab9bc1dbbd24e84b752dcc036e289
Sarah Mogin <mogin@work.co>
2016-12-23 22:14:10 -0500

    wow I did it

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -3,15 +3,11 @@ import '../../../deps/phoenix/priv/static/phoenix'
 import '../../../deps/phoenix_html/priv/static/phoenix_html'
 
 // Shiny new, hot React component
-import React, { Component } from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
-class Root extends Component {
-  render () {
-    return (
-      <h1>omg so hot</h1>
-    );
-  }
+function Root() {
+  return <h1>Hi</h1>;
 }
 
 render(<Root />, document.getElementById('root'))
